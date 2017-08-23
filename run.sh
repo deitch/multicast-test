c6c07ce320b24087691adb015ae7a738d8e269fb
Avi Deitcher <avi@deitcher.net>
2017-08-23 20:08:06 +0300

    Wait long enough for server to start

--- run.sh
+++ run.sh
@@ -3,16 +3,15 @@
 MADDR=224.16.17.8
 PORT=$(($RANDOM%8000+2001))
 
-# Find an unused multicast address /32 beginning with 
+# Find an unused multicast address /32 beginning with
 route add ${MADDR}/32 via 127.0.0.1 dev lo
 
 ./dist/server ${MADDR} ${PORT} &
 SERVER_PID=$!
+# need to sleep long enough for server to be listening
+sleep 5
 ./dist/client ${MADDR} ${PORT}
 
 kill $SERVER_PID
 
 route del ${MADDR}/32 via 127.0.0.1 dev lo
-
-
-
